bc6641ac9a1499887e59d7f1f87a5207581fb0cc
jefer94 <jdefreitaspinto@gmail.com>
2020-03-26 00:45:41 -0500

    add todo

--- packages/algorithm/index.js
+++ packages/algorithm/index.js
@@ -3,4 +3,16 @@ import express from 'express'
 const app = express()
 const port = process.env.PORT || 3000
 
+/**
+ * @todo Add Mongoose.
+ * @todo Persist tabs
+ * @todo Persist code
+ * @todo Persist lang
+ * @todo Save project and load
+ * @todo Cache
+ * @todo Security headers
+ * @todo Users and tokens
+ * @todo Tests and docs 
+ */
+
 app.listen(port)
