51693431ee3a8f87486d8ae7a79e5ef2659342fb
jefer94 <jdefreitaspinto@gmail.com>
2020-07-20 14:02:22 -0500

    add commonjs codemirror version

--- packages/algorithm/next.config.js
+++ packages/algorithm/next.config.js
@@ -1,3 +1,13 @@
+// import { loadEnv } from '@choco/env'
+
+// loadEnv()
+
+// export default {
+//   env: {
+//     HUB_API: process.env.HUB_API
+//   }
+// }
+
 const { loadEnv } = require('@choco/env')
 
 loadEnv()
---
+++ scripts/development/codemirror.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env sh
+
+# clone
+mkdir libs
+cd libs
+rm -R codemirror
+git clone https://github.com/codemirror/codemirror.next codemirror
+cd codemirror
+
+# last lag
+latesttag=$(git describe --tags)
+echo checking out ${latesttag}
+git checkout ${latesttag}
+
+# setup to module commonjs
+sed -i 's/ "es6"/ "es5"/g' tsconfig.base.json
+# sed -i 's/ "@codemirror/next"/ "@choco/codemirror"/g' package.json
+sed -i '/"."/d' package.json
+sed -i '/"type": "module"/d' package.json
+
+# add dist folder
+sed -i '5i    "outDir": "./dist",' tsconfig.json
+
+# install typescript
+yarn global add typescript
+
+# setup internal module to commonjs and build
+for folder in *; do
+  if [ -d $folder ] && [ -d $folder/src ]; then
+    echo src $folder
+  elif [ -d $folder ] && [ -f $folder/package.json ]; then
+    echo package $folder
+  else
+    echo nobody $folder
+  fi
+
+  if [ -d $folder ] && [ -d $folder/src ]; then
+    cd $folder
+    sed -i 's/ "module"/ "commonjs"/g' package.json
+    cp ../tsconfig.json ./tsconfig.json
+    sed -i 's/"\*\//"/g' tsconfig.json
+    sed -i 's/, "test\/\*\.ts"//g' tsconfig.json
+    sed -i 's/, "demo\/demo\.ts"//g' tsconfig.json
+    cp ../tsconfig.base.json ./tsconfig.base.json
+    tsc --build ./tsconfig.json
+    cd ..
+  fi
+done
+
+# yarn
+# yarn prepare
+
+cd ../..
+yarn
