7ed69abe178f40eca3eba92769950261d4991894
jefer94 <jdefreitaspinto@gmail.com>
2020-08-04 05:16:44 -0500

    fix tests

--- packages/algorithm-transpiler/src/comments.test.ts
+++ packages/algorithm-transpiler/src/comments.test.ts
@@ -1,22 +1,16 @@
 import comments from './comments'
 
 test('remove comments from a line', () => {
-  expect(comments('hi // Hi Hi Puffy AmiYumi')).toBe('hi')
+  const result = comments('hi // Hi Hi Puffy AmiYumi')
+  expect(result).toBe('hi')
 })
 
 test('remove comments from a lines (array)', () => {
-  testMultilinesResultInArray(comments(['hi // Teens Titans Go', 'apple // Steven Universe']))
+  const result = comments(['hi // Teens Titans Go', 'apple // Steven Universe'])
+  expect(result).toEqual(['hi', 'apple'])
 })
 
 test('remove comments from a lines (breaklines)', () => {
   const result = comments(['hi //Gravity Falls', 'apple //South Park'].join('\n'))
-  testMultilinesResultInArray(result.split('\n'))
+  expect(result).toBe('hi\napple')
 })
-
-function testMultilinesResultInArray(result) {
-  const [hi, apple, ...restOfStuff] = result
-
-  expect(restOfStuff).toHaveLength(0)
-  expect(hi).toBe('hi')
-  expect(apple).toBe('apple')
-}
