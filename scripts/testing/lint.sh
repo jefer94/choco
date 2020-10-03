c74a318fcffedbf729e16c19626d0fcc0d955f32
jefer94 <jdefreitaspinto@gmail.com>
2020-10-02 21:53:16 -0500

    add circleci

--- scripts/testing/lint.sh
+++ scripts/testing/lint.sh
@@ -1,41 +1,46 @@
 #!/usr/bin/env sh
 
-# cd ./packages
+grey=$(tput setaf 0)
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+yelloc=$(tput setaf 3)
+blue=$(tput setaf 4)
+violet=$(tput setaf 5)
+cyan=$(tput setaf 6)
+white=$(tput setaf 7)
+normal=$(tput sgr0)
 
-# if [ $1 ]; then
-#   cd $1
-#   if ! yarn eslint src/**/* $2; then
-#     exit 1
-#   fi
-#   cd ..
-# else
-#   for i in *; do
-#     cd $i
-#     if ! yarn eslint src/**/*; then
-#       exit 1
-#     fi
-#     cd ..
-#   done
-# fi
-
-# cd ..
-
-cd ./services
-
-if [ $1 ]; then
+lint() {
   cd $1
+  echo "<-------- $1 -------->"
   if ! yarn eslint src/**/* $2; then
+
+    printf "\n"
+    printf '%0s\n' "${red}'$1' package/service has failed${normal}"
+    printf "\n"
     exit 1
   fi
   cd ..
-else
-  for i in *; do
-    cd $i
-    if ! yarn eslint src/**/*; then
-      exit 1
-    fi
-    cd ..
-  done
-fi
+}
 
-cd ..
+if [ $1 ]; then
+  if [ -d ./packages/$1 ]; then
+    cd ./packages
+    lint $1 $2
+  else
+    cd ./services
+    lint $1 $2
+  fi
+else
+  cd ./packages
+  for folder in *; do
+    lint $folder
+  done
+  cd ..
+
+  cd ./services
+  for folder in *; do
+    lint $folder
+  done
+  cd ..
+fi
